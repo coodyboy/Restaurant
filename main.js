33f75e157812d045aae5bd67a6045c4713fa5869
coodyboy <151982939+coodyboy@users.noreply.github.com>
2023-12-27 21:16:39 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+function displaySidebar(){
+    document.getElementById('side-bar').style.display = 'block'
+}
+
+function hideSidebar(){
+    document.getElementById('side-bar').style.display = 'none'
+}
+
+function alertYou(){
+    alert(`We know you can't afford it.`)
+}
+console.log(`What are you up to?`)
